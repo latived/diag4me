c66da49261925aef24b153b913c295571541b82d
vitalwarley <vitalwarley@gmail.com>
2020-02-05 17:29:32 -0300

    Add login/signup form

--- diagnose4me/lib/main.dart
+++ diagnose4me/lib/main.dart
@@ -13,7 +13,15 @@ class LoginSignUpPage extends StatefulWidget {
 
 class _LoginSignUpPageState extends State<LoginSignUpPage> {
 
-  var _isLoading = true;
+  var _isLoading = false;
+  var _isLoginForm = true;
+
+  var _errorMessage = "";
+
+  var _email;
+  var _password;
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +31,7 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
       ),
       body: Stack(
         children: <Widget>[
-          //showForm(),
+         _showForm(),
           showCircularProgress(),
         ],
       ),
@@ -40,6 +48,152 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
       height: 0.0,
       width: 0.0,
     );
+  }
+
+  Widget showLogo() {
+    return Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 70, 0, 0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/flutter-icon.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+      child: TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          icon: Icon(
+            Icons.mail,
+            color: Colors.grey,
+          )
+        ),
+        validator: (value) => value.isEmpty ? "Email can't be empty" : null,
+        onSaved: (value) => _email = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+      child: TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          icon: new Icon(
+            Icons.lock,
+            color: Colors.grey,
+          )
+        ),
+        validator: (value) => value.isEmpty ? "Password can't be empty" : null,
+        onSaved: (value) => _password = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPrimaryButton() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 45, 0, 0),
+      child: SizedBox(
+        height: 40.0,
+        child: RaisedButton(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          color: Colors.blue,
+          child: new Text(
+            _isLoginForm ? 'Login' : 'Create account',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+          onPressed: _validateAndSubmit,
+        ),
+      ),
+    );
+  }
+
+  Widget showSecondaryButton() {
+    return FlatButton(
+      child: Text(
+        _isLoginForm ? 'Create an account' : 'Have an account? Sign in',
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w300
+        ),
+      ),
+      onPressed: _toggleFormMode,
+    );
+  }
+
+  Widget showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return Text(
+        _errorMessage,
+        style: TextStyle(
+          fontSize: 13.0,
+          color: Colors.red,
+          height: 1.0,
+          fontWeight: FontWeight.w300,
+        ),
+      );
+    } else {
+      return Container(
+        height: 0.0,
+      );
+    }
+  }
+
+  Widget _showForm() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            showLogo(),
+            showEmailInput(),
+            showPasswordInput(),
+            showPrimaryButton(),
+            showSecondaryButton(),
+            showErrorMessage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _validateAndSubmit() {
+    print('login/validate off right now');
+    return null;
+  }
+
+  Widget _toggleFormMode() {
+    _resetForm();
+    setState(() {
+      _isLoginForm = !_isLoginForm;
+    });
+  }
+
+  Widget _resetForm() {
+    print('resetForm off right now');
+    return null;
   }
 }
 
