04cdfa412107d33095f4509f8da086b1a1f856ca
vitalwarley <vitalwarley@gmail.com>
2020-02-12 18:19:22 -0300

    Adds button to open the chat

--- diagnose4me/lib/pages/exam_detail_page.dart
+++ diagnose4me/lib/pages/exam_detail_page.dart
@@ -18,9 +18,42 @@ class ExameDetailPage extends StatelessWidget {
               Text(details[0]),
               Text(details[1]),
               Text(details[2]),
+              showPrimaryButton()
           ],
         ),
       ),
     );
   }
+
+  Widget showPrimaryButton() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 45, 0, 0),
+      child: SizedBox(
+        height: 40.0,
+        child: RaisedButton(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          color: Colors.blue,
+          child: new Text(
+            'Help me understand it',
+            style: TextStyle(
+              fontSize: 10.0,
+              color: Colors.white,
+            ),
+          ),
+          onPressed: () {
+            print('Chat! Chat!');
+            //Navigator.push(context, MaterialPageRoute(
+            //    builder: (context) => ExameDetailPage(details: details,)
+            //));
+          }
+        ),
+      ),
+    );
+  }
+
+
+
 }
